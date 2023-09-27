9212d25262c0c45eac6be5878854aca9cbe2793b
Samvel Israelyan <sisraelyan@vmware.com>
2023-09-27 09:55:06 +0400

    Added demo tests

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+    Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y and returns result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtracts y from x and returns result."""
+    return x - y
