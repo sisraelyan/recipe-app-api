156a1d0eaa53321d6eff9e1155939ed61ff8e8ef
Samvel Israelyan <sisraelyan@vmware.com>
2023-09-29 10:18:52 +0400

    We have the database set up

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,7 +2,11 @@
 
 from django.core.management.base import BaseCommand
 
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
+
 class Command(BaseCommand):
     """Django command to wait for database"""
     def handle(self, *args, **options):
-        pass
+        self.stdout.write("Waiting for database...")
